bde331223d8af8c09551538455fc761b102640fe
“SzmitP” <szmitp@oriax.eu>
2023-05-02 15:16:14 +0200

    added empty endpoints for a user controller

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.GenericEntityDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ecommercee/users")
 public class UserController {
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public GenericEntityDto addUser(@RequestBody GenericEntityDto genericEntityDto) {
+        return genericEntityDto;
+    }
+
+    @PutMapping(value = "{id}/block")
+    public GenericEntityDto blockUser(@PathVariable("id") Long id) {
+        return new GenericEntityDto(id,"user is blocked");
+    }
+
+    @PutMapping(value = "{id}/generate")
+    public GenericEntityDto generateKey(@PathVariable("id") Long id) {
+        return new GenericEntityDto(id, "key generated");
+    }
 }
