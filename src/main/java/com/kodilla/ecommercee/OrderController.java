85e034900209806ff904fda3279d7b2389e0fa57
Paulina Borym <paulina.borzym@op.pl>
2023-05-02 12:54:37 +0200

    add empty OrderDto,add endpoints to OrderController

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.OrderDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/v1/orders")
 public class OrderController {
+    @GetMapping
+    public List<OrderDto> getOrders(){
+        return new ArrayList<>();
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createOrder(@RequestBody OrderDto orderDto){
+
+    }
+    @GetMapping(value = "{OrderId}")
+    public OrderDto getOrder(@PathVariable int OrderId) {
+        return new OrderDto();
+    }
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public OrderDto updateOrder(@RequestBody OrderDto orderDto){
+        return new OrderDto();
+    }
+
+    @DeleteMapping(value = "{OrderId}")
+    public void deleteOrder(@PathVariable int OrderId){
+
+    }
 }
