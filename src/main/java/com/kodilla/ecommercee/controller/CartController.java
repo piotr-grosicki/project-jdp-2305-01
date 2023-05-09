28d35908d181d26f3e78d8eaddee89318882646a
Pakos2137 <michal.pakos@Onet.pl>
2023-05-09 21:25:28 +0200

    JDP230501-3

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -8,27 +8,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/carts")
+@RequestMapping("/v1/cart")
 public class CartController {
-
-    @GetMapping
-    public List<CartDto> getCarts() {
-        return new ArrayList<>();
-    }
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public CartDto addCart(@RequestBody CartDto cartDto)  {
+    public CartDto createCart(@RequestBody CartDto cartDto)  {
         return cartDto;
     }
-    @GetMapping(value = "{cartId}")
-    public CartDto updateCart(@PathVariable int cartId) {
-        return new CartDto();
-    }
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public CartDto updateCart(@RequestBody CartDto cartDto) {
+    @PostMapping(value = "{cartId}/{productId}")
+    public CartDto addToCart(@PathVariable int cartId, @PathVariable int productId) {
         return new CartDto();
     }
     @DeleteMapping(value = "{cartId}")
     public void deleteCart(@PathVariable int cartId) {
+    }
+    @DeleteMapping(value = "{cartId}/{productId}")
+    public void deleteFromCart(@PathVariable int cartId, @PathVariable int productId) {
+    }
+    @GetMapping(value = "{cartId}/{productId}")
+    public CartDto getProduct(@PathVariable int cartId, @PathVariable int productId) {
+        return new CartDto();
+    }
+    @GetMapping
+    public List<CartDto> getProductsList() {
+        return new ArrayList<>();
     }
 }
 
--- src/main/java/com/kodilla/ecommercee/dto/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/CartDto.java
@@ -1,4 +1,18 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class CartDto {
+    private long id;
+    private long userId;
+    private BigDecimal Total_Price;
 }
