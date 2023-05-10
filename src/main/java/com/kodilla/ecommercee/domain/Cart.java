36c66230cb4ffebddbfeb82bfeb1b7d0c20996ab
Pakos2137 <michal.pakos@Onet.pl>
2023-05-10 18:39:38 +0200

    JDP230501-14

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Setter
 @AllArgsConstructor
