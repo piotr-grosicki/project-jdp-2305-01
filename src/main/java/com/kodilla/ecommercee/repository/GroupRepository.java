292ad927fc44d9e8b1118b786052592f85793291
“SzmitP” <szmitp@oriax.eu>
2023-05-14 19:54:37 +0200

    minor changes added

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -4,18 +4,14 @@ import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface GroupRepository extends CrudRepository<Group,Long> {
-    @Override
     List<Group> findAll();
-
-    @Override
-    Group save(Group group);
-
-    @Override
-    Optional<Group> findById(Long id);
-
+    Group save(final Group group);
+    Group findByGroupId(final long groupId);
 }
