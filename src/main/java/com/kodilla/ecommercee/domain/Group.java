96efa31264c588cec000783ac5ac75e290f52a58
“SzmitP” <szmitp@oriax.eu>
2023-05-14 19:48:15 +0200

    add relations

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -9,13 +9,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "GROUPS")
 public class Group {
-    Long groupId;
-    String groupName;
+    private Long groupId;
+    private String groupName;
+    private List<Product> productList = new ArrayList<>();
+
+    public Group(final String groupName) {
+        this.groupName = groupName;
+    }
+
     @Id
     @GeneratedValue
     @Column(name = "GROUP_ID", unique = true)
@@ -26,5 +31,14 @@ public class Group {
     @Column(name = "GROUP_NAME", unique = true)
     public String getGroupName() {
         return groupName;
+    }
+
+    @OneToMany(
+            targetEntity = Product.class,
+            cascade = CascadeType.ALL,
+            mappedBy = "group",
+            fetch = FetchType.LAZY)
+    public List<Product> getProductList() {
+        return productList;
     }
 }
