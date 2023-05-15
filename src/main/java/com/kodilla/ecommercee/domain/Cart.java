ecbaea2931c492de3825e18665369c1095402f49
Pakos2137 <michal.pakos@Onet.pl>
2023-05-15 16:30:32 +0200

    JDP230501-14

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,39 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "CART")
+public class Cart {
+    Long cartId;
+    BigDecimal totalPrice;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "CART_ID",unique = true)
+    public Long getCartId() {
+        return cartId;
+    }
+    @NotNull
+    @Column(name = "TOTAL_PRICE")
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+    /*
+    @ManyToMany(
+            cascade = CascadeType.REFRESH,
+            fetch = FetchType.LAZY)
+    )
+    private List<Product> productsList = new ArrayList<>();
+
+     */
+}
