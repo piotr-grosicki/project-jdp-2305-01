500078acc8dc21a0896110ebffc349c5fc3761d4
“SzmitP” <szmitp@oriax.eu>
2023-05-19 10:12:09 +0200

    Add tests

--- src/test/java/com/kodilla/ecommercee/ProductEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/ProductEntityTestSuite.java
@@ -6,15 +6,18 @@ import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
+
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
-@SpringBootTest()
+@DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class ProductTestSuite {
+public class ProductEntityTestSuite {
 
     @Autowired
     private ProductRepository productRepository;
@@ -22,7 +25,7 @@ public class ProductTestSuite {
     private GroupRepository groupRepository;
 
     @Test
-    public void shouldReturnAllProducts() {
+    public void returnAllProducts() {
         // given
         Group group = new Group("Group");
 
@@ -30,15 +33,16 @@ public class ProductTestSuite {
                 "Product1",
                 "Description1",
                 10,
-                new BigDecimal(10),
-                group);
+                new BigDecimal(10));
 
         Product product2 = new Product(
                 "Product2",
                 "Description2",
                 20,
-                new BigDecimal(20),
-                group);
+                new BigDecimal(20));
+
+        product1.setGroup(group);
+        product2.setGroup(group);
 
         group.getProductList().addAll(Arrays.asList(product1,product2));
 
@@ -46,12 +50,12 @@ public class ProductTestSuite {
         groupRepository.save(group);
 
         // then
-        assertEquals(2,productRepository.findAll().size());
+        assertEquals(2,productRepository.count());
 
     }
 
     @Test
-    public void shouldReturnOneProduct() {
+    public void returnOneProduct() {
         // given
         Group group = new Group("Group");
 
@@ -59,15 +63,16 @@ public class ProductTestSuite {
                 "Product1",
                 "Description1",
                 10,
-                new BigDecimal(10),
-                group);
+                new BigDecimal(10));
 
         Product product2 = new Product(
                 "Product2",
                 "Description2",
                 20,
-                new BigDecimal(20),
-                group);
+                new BigDecimal(20));
+
+        product1.setGroup(group);
+        product2.setGroup(group);
 
         group.getProductList().addAll(Arrays.asList(product1,product2));
 
@@ -79,28 +84,29 @@ public class ProductTestSuite {
     }
 
     @Test
-    public void shouldCreateNewProduct() {
+    public void createNewProduct() {
         // given
-        Group group = new Group("Group");
-
         Product product = new Product(
                 "Product1",
                 "Description1",
                 10,
-                new BigDecimal(10),
-                group);
+                new BigDecimal(10));
 
-        group.getProductList().add(product);
+        product.setGroup(null);
 
         // when
-        groupRepository.save(group);
+        productRepository.save(product);
+        Product expectedProduct = productRepository.findByProductId(product.getProductId());
 
         // then
-        assertEquals(1, productRepository.count());
+        assertEquals("Product1",expectedProduct.getProductName());
+        assertEquals("Description1",expectedProduct.getProductDescription());
+        assertEquals(10, expectedProduct.getProductQuantity());
+        assertEquals(BigDecimal.valueOf(10).setScale(2), expectedProduct.getProductPrice().setScale(2));
     }
 
     @Test
-    public void shouldUpdateProduct() {
+    public void updateProduct() {
         // given
         Group group = new Group("Group");
 
@@ -108,15 +114,16 @@ public class ProductTestSuite {
                 "Product1",
                 "Description1",
                 10,
-                new BigDecimal(10),
-                group);
+                new BigDecimal(10));
 
         Product product2 = new Product(
                 "Product2",
                 "Description2",
                 20,
-                new BigDecimal(20),
-                group);
+                new BigDecimal(20));
+
+        product1.setGroup(group);
+        product2.setGroup(group);
 
         group.getProductList().addAll(Arrays.asList(product1,product2));
 
@@ -138,7 +145,7 @@ public class ProductTestSuite {
     }
 
     @Test
-    public void shouldDeleteOneProduct() {
+    public void deleteOneProduct_ShouldKeepAllGroups() {
         // given
         Group group = new Group("Group");
 
@@ -146,28 +153,36 @@ public class ProductTestSuite {
                 "Product1",
                 "Description1",
                 10,
-                new BigDecimal(10),
-                group);
+                new BigDecimal(10));
 
         Product product2 = new Product(
                 "Product2",
                 "Description2",
                 20,
-                new BigDecimal(20),
-                group);
+                new BigDecimal(20));
+
+        product1.setGroup(group);
+        product2.setGroup(group);
 
         group.getProductList().addAll(Arrays.asList(product1,product2));
+        groupRepository.save(group);
 
         // when
-        groupRepository.save(group);
+        Product productReadFromDb = productRepository.findByProductId(product1.getProductId());
+        Group groupReadFromProduct = productReadFromDb.getGroup();
+
+        groupReadFromProduct.getProductList().remove(productReadFromDb);
+        groupRepository.save(groupReadFromProduct);
+
         productRepository.deleteById(product1.getProductId());
 
         // then
         assertEquals(1,productRepository.count());
+        assertEquals(1,groupRepository.count());
     }
 
     @Test
-    public void shouldRemoveAllProductsButGroupsShouldStay() {
+    public void deleteAllProducts_shouldKeepAllGroups() {
         // given
         Group group = new Group("Group");
 
@@ -175,54 +190,33 @@ public class ProductTestSuite {
                 "Product1",
                 "Description1",
                 10,
-                new BigDecimal(10),
-                group);
+                new BigDecimal(10));
 
         Product product2 = new Product(
                 "Product2",
                 "Description2",
                 20,
-                new BigDecimal(20),
-                group);
+                new BigDecimal(20));
 
-        group.getProductList().addAll(Arrays.asList(product1,product2));
+        product1.setGroup(group);
+        product2.setGroup(group);
+
+        group.getProductList().add(product1);
+        group.getProductList().add(product2);
+
+        groupRepository.save(group);
 
         // when
-        groupRepository.save(group);
+        List<Group> groupsReadFromDb = groupRepository.findAll();
+
+        groupsReadFromDb.stream()
+                .peek(g -> group.getProductList().clear())
+                .forEach(groupRepository::save);
+
         productRepository.deleteAll();
 
         // then
-        assertEquals(0,productRepository.findAll().size());
-        assertEquals(1,groupRepository.findAll().size());
-    }
-
-    @Test
-    public void shouldRemoveGroupAndAllProducts() {
-        // given
-        Group group = new Group("Group");
-
-        Product product1 = new Product(
-                "Product1",
-                "Description1",
-                10,
-                new BigDecimal(10),
-                group);
-
-        Product product2 = new Product(
-                "Product2",
-                "Description2",
-                20,
-                new BigDecimal(20),
-                group);
-
-        group.getProductList().addAll(Arrays.asList(product1,product2));
-
-        // when
-        groupRepository.save(group);
-        groupRepository.deleteAll();
-
-        // then
-        assertEquals(0,groupRepository.findAll().size());
-        assertEquals(0,productRepository.findAll().size());
+        assertEquals(0,productRepository.count());
+        assertEquals(1,groupRepository.count());
     }
 }
